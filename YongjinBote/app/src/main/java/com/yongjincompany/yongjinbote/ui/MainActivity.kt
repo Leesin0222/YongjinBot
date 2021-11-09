a75c773b857b96f1e2d88cfdb8ba3c60a1d5bc5b
Leesin0222 <dragon02229@naver.com>
2021-11-09 17:31:37 +0900

    🪢 :: ui파일 만들고 MainActivity ui파일로 이동

--- YongjinBote/app/src/main/java/com/yongjincompany/yongjinbote/ui/MainActivity.kt
+++ YongjinBote/app/src/main/java/com/yongjincompany/yongjinbote/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.yongjincompany.yongjinbote
+package com.yongjincompany.yongjinbote.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yongjincompany.yongjinbote.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

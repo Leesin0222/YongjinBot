70138b90c2a183443aca4017f7362f2f1102562f
Leesin0222 <dragon02229@naver.com>
2021-11-11 14:40:06 +0900

    👍 ::MainActivity 완성

--- YongjinBote/app/src/main/java/com/yongjincompany/yongjinbote/ui/MainActivity.kt
+++ YongjinBote/app/src/main/java/com/yongjincompany/yongjinbote/ui/MainActivity.kt
@@ -1,12 +1,126 @@
 package com.yongjincompany.yongjinbote.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.yongjincompany.yongjinbote.R
+import com.yongjincompany.yongjinbote.utils.BotResponse
+import com.yongjincompany.yongjinbote.utils.Constants.OPEN_GOOGLE
+import com.yongjincompany.yongjinbote.utils.Constants.OPEN_SEARCH
+import com.yongjincompany.yongjinbote.utils.Constants.RECEIVE_ID
+import com.yongjincompany.yongjinbote.utils.Constants.SEND_ID
+import com.yongjincompany.yongjinbote.utils.Time
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: MessagingAdapter
+    private val botList = listOf("김민성", "이용진", "철수", "유미")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView()
+
+        clickEvents()
+
+        val random = (0..3).random()
+        customMessage("안녕? 오늘 너와 얘기할 ${botList[random]}이야, 나한테 할 말이 있니?")
+    }
+
+    private fun clickEvents() {
+        btn_send.setOnClickListener {
+            sendMessage()
+        }
+
+        et_message.setOnClickListener {
+            GlobalScope.launch {
+                delay(100)
+                withContext(Dispatchers.Main) {
+                    rv_messages.scrollToPosition(adapter.itemCount -1)
+                }
+            }
+        }
+    }
+
+    private fun recyclerView() {
+        adapter = MessagingAdapter()
+        rv_messages.adapter = adapter
+        rv_messages.layoutManager = LinearLayoutManager(applicationContext)
+    }
+
+    private fun sendMessage() {
+        val message = et_message.text.toString()
+        val timeStamp = Time.timeStamp()
+
+        if(message.isNotEmpty()) {
+            et_message.setText("")
+
+            adapter.insertMessage(com.yongjincompany.yongjinbote.data.Message(message, SEND_ID, timeStamp))
+            rv_messages.scrollToPosition(adapter.itemCount -1)
+
+            botResponse(message)
+        }
+    }
+
+    private fun botResponse(message: String) {
+        val timeStamp = Time.timeStamp()
+
+        GlobalScope.launch {
+            delay(1000)
+
+            withContext(Dispatchers.Main){
+                val response = BotResponse.basicResponses(message)
+
+                adapter.insertMessage(com.yongjincompany.yongjinbote.data.Message(response, RECEIVE_ID, timeStamp))
+                rv_messages.scrollToPosition(adapter.itemCount -1)
+
+                when(response) {
+                    OPEN_GOOGLE -> {
+                        val site = Intent(Intent.ACTION_VIEW)
+                        site.data = Uri.parse("https://www.google.com/")
+                        startActivity(site)
+
+                    }
+                    OPEN_SEARCH -> {
+                        val site = Intent(Intent.ACTION_VIEW)
+                        val searchTerm: String? = message.substringAfter("search")
+                        site.data = Uri.parse("https://www.google.com/search?&q=$searchTerm")
+                        startActivity(site)
+                    }
+                }
+            }
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        GlobalScope.launch {
+            delay(1000)
+            withContext(Dispatchers.Main) {
+                val timeStamp = Time.timeStamp()
+                adapter.insertMessage(com.yongjincompany.yongjinbote.data.Message(message = String(), RECEIVE_ID, timeStamp))
+
+                rv_messages.scrollToPosition(adapter.itemCount -1)
+            }
+        }
+    }
+
+    private fun customMessage(message: String) {
+        GlobalScope.launch {
+            delay(1000)
+            withContext(Dispatchers.Main){
+                val timestamp = Time.timeStamp()
+                adapter.insertMessage(com.yongjincompany.yongjinbote.data.Message(message, RECEIVE_ID, timestamp))
+
+                rv_messages.scrollToPosition(adapter.itemCount-1)
+            }
+        }
     }
 }

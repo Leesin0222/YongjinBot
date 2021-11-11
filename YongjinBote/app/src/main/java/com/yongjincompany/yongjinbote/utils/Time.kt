b8305291179525eb0fef0e7ffa095f08de9429e5
Leesin0222 <dragon02229@naver.com>
2021-11-11 14:40:47 +0900

    📑 :: Time 완성

--- YongjinBote/app/src/main/java/com/yongjincompany/yongjinbote/utils/Time.kt
+++ YongjinBote/app/src/main/java/com/yongjincompany/yongjinbote/utils/Time.kt
@@ -1,4 +1,16 @@
 package com.yongjincompany.yongjinbote.utils
 
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.*
+
 object Time {
+
+    fun timeStamp(): String {
+        val timeStamp = Timestamp(System.currentTimeMillis())
+        val sdf = SimpleDateFormat("HH:mm")
+        val time = sdf.format(Date(timeStamp.time))
+
+        return time.toString()
+    }
 }

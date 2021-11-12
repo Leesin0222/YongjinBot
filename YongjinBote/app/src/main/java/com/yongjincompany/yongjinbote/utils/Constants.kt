ed3b8bef8f416f362b6389a03f483a889f6cc672
Leesin0222 <dragon02229@naver.com>
2021-11-12 22:46:55 +0900

    📑 :: OPEN_GOOGLE, OPEN_SEARCH 시 뜨는 채팅 변경

--- YongjinBote/app/src/main/java/com/yongjincompany/yongjinbote/utils/Constants.kt
+++ YongjinBote/app/src/main/java/com/yongjincompany/yongjinbote/utils/Constants.kt
@@ -5,6 +5,6 @@ object Constants {
     const val SEND_ID = "SEND_ID"
     const val RECEIVE_ID = "RECEIVE_ID"
 
-    const val OPEN_GOOGLE = "Opening Google"
-    const val OPEN_SEARCH = "Searching"
+    const val OPEN_GOOGLE = "구글 접속 중..."
+    const val OPEN_SEARCH = "검색 중..."
 }
